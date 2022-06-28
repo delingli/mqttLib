21a239abc72858d40fe8c5ca691e801b9f387d95
deling <lideling911911@163.com>
2022-06-28 13:57:38 +0800

    提交视拓类型

--- switchDeviceComponent/src/main/java/com/itc/switchdevicecomponent/annation/DeviceType.kt
+++ switchDeviceComponent/src/main/java/com/itc/switchdevicecomponent/annation/DeviceType.kt
@@ -8,13 +8,15 @@ class DeviceType {
         const val MODULE_JINGXIN = "JINGXIN"  //精鑫
         const val MODULE_SHRG = "SHRG"  //深海瑞格
         const val MODULE_ANDROID_HE_ZI = "ANDROID_HE_ZI"  //智微Android盒子
+        const val MODULE_SK = "ANDROID_SK"  //视拓
     }
 
     @StringDef(
         MODULE_SHRG,
         MODULE_HEZI0830B,
         MODULE_JINGXIN,
-        MODULE_ANDROID_HE_ZI
+        MODULE_ANDROID_HE_ZI,
+        MODULE_SK
     )
     @Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FIELD, AnnotationTarget.FUNCTION)
     @Retention(AnnotationRetention.SOURCE)
